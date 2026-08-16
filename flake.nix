f1623782ac6500cc075e8009849de677c130ddda
rkarsnk <26945036+rkarsnk@users.noreply.github.com>
2026-08-16 16:54:17 +0900

    add nixos directory.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixosConfigurations.nixserv = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./nixos/configuration.nix
       ];
     };
   };
